c32284529ecfb989ffa3b37945be5f46d2af139a
Anjali Sood <anjali.sood@intel.com>
2016-04-25 15:26:27 -0700

    Added logging

--- scoring_pipelines/record.py
+++ scoring_pipelines/record.py
@@ -25,11 +25,15 @@ import sys
 from atktypes import valid_data_types
 import requests
 from collections import OrderedDict
+import logging
 
+
+logging.basicConfig(filename='scoringPipeline.log', level=logging.DEBUG)
 
 class Record(MutableRow):
 
     def add_columns(self, user_func, schema):
+        logging.debug('Add')
         result = user_func(self)
         self._data = self._data + result
         if len(schema) == len(result):
@@ -39,16 +43,19 @@ class Record(MutableRow):
                 self._dtype_constructors.append(valid_data_types.get_constructor(value))
                 self._indices_dict[key] = len(self._indices_dict)
         else:
-            pass
             # log a warning and not do anything
+	    logging.warn('length of schema does not match with the length of the result.')
+	    pass
 
 
     def filter(self, user_func):
+	logging.debug('filter')
         return user_func(self)
 
 
 
     def drop_columns(self, col_list):
+        logging.debug('drop_columns')
         indices = []
         try:
             for i in col_list:
@@ -64,15 +71,18 @@ class Record(MutableRow):
             self._indices_dict = dict([(k, i) for i, k, in enumerate(self._schema_dict.keys())])
         except Exception as e:
             message = "Failed to drop all columns %s" %e
+	    logging.error(message)
             raise IOError(message)
 
     def score(self, uri):
+	logging.debug('score')
         s = "http://" + uri + "/v1/score?data=" + ','.join(str(d) for d in self._data )
         return (requests.post(s).text )
 
 
 
     def rename_columns(self, column_list):
+	logging.debug('rename_columns')
         for old_key, new_key in column_list.iteritems():
             new_dict = OrderedDict([(new_key, v) if k == old_key else (k,v) for k,v in self._schema_dict.items()])
             self._schema_dict = new_dict
